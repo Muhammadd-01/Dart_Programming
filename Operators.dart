c8488542b5cf624214f22ba8c0ae065aa6d9364c
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-05-13 14:14:41 +0500

    Dart Programming

--- Operators.dart
+++ Operators.dart
@@ -1,4 +1,10 @@
-void Main(){
+void main() {
+  int a = 10;
+  int b = 20;
 
-  
+  print("Addition: ${a + b}");
+  print("Subtraction: ${a - b}");
+  print("Mulitiplication: ${a * b}");
+  print("Division: ${a / b}");
+  print("Modulus: ${a % b}");
 }
