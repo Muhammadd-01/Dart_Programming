aa5ec8043ce802a5eb64de017dce94804b3bc446
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-05-13 14:10:50 +0500

    Dart Programming

--- DataTypes.dart
+++ DataTypes.dart
@@ -1,35 +1,35 @@
 void main() {
-  // String Name = 'Affan'; // Type = String
+  String Name = 'Affan'; // Type = String
 
-  // int Age = 20; // Type = int
+  int Age = 20; // Type = int
 
-  // double Height = 6.1;  // Type = double
+  double Height = 6.1;  // Type = double
 
-  // bool isMarried = false; // Type = bool
+  bool isMarried = false; // Type = bool
 
-  // print("My Name is $Name and Type is ${Name.runtimeType}");
-  // print("My Age is $Age and Type is ${Age.runtimeType}");
-  // print("My Height is $Height and Type is ${Height.runtimeType}");
-  // print("My Marital Status is $isMarried and Type is ${isMarried.runtimeType}");
+  print("My Name is $Name and Type is ${Name.runtimeType}");
+  print("My Age is $Age and Type is ${Age.runtimeType}");
+  print("My Height is $Height and Type is ${Height.runtimeType}");
+  print("My Marital Status is $isMarried and Type is ${isMarried.runtimeType}");
 
   // DIfference between dynamic and var
 
-  // var a = "Affan";
-  // print(a);
+  var a = "Affan";
+  print(a);
 
-  // a = 12; // This will give an error because 'a' is declared as a String and cannot be reassigned to an int type value because 'var' is a static and final type
+  a = 12; // This will give an error because 'a' is declared as a String and cannot be reassigned to an int type value because 'var' is a static and final type
 
-  // dynamic b = 20;
-  // print(b);
+  dynamic b = 20;
+  print(b);
 
-  // b = "Affan"; // This will not give an error because 'b' is declared as a dynamic type and can be reassigned to any type of value because 'dynamic' is a dynamic type
+  b = "Affan"; // This will not give an error because 'b' is declared as a dynamic type and can be reassigned to any type of value because 'dynamic' is a dynamic type
 
-  // dynamic Name = "Affan";
-  // Name = 20;
+  dynamic Name = "Affan";
+  Name = 20;
 
-  // Name = 6.1;
+  Name = 6.1;
 
-  // print(Name.runtimeType); // This will print the type of the variable 'Name' which is dynamic and if you check the type of the variable it will show the last assigned type which is double
+  print(Name.runtimeType); // This will print the type of the variable 'Name' which is dynamic and if you check the type of the variable it will show the last assigned type which is double
 
   dynamic a = 1234;
 
