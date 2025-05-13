0a1ca713107273187c451e5ed796239d8132842d
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-05-13 13:58:25 +0500

    Dart Programming

--- DataTypes.dart
+++ DataTypes.dart
@@ -1,3 +1,14 @@
 void main() {
-Name : Affan -> Type : String;
+  String Name = 'Affan'; // Type = String
+
+  int Age = 20; // Type = int
+
+  double Height = 6.1;  // Type = double
+
+  bool isMarried = false; // Type = bool
+
+  print("My Name is ${Name.runtimeType}");
+  print("My Age is ${Age.runtimeType}");
+  print("My Height is ${Height.runtimeType}");
+  print("My Marital Status is ${isMarried.runtimeType}");
 }
