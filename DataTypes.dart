e54323768ccb205829d33f73d88e4e8b78a0a021
Muhammadd-01 <Muhammadaffan1445@gmail.com>
2025-05-13 14:03:20 +0500

    Dart Programming

--- DataTypes.dart
+++ DataTypes.dart
@@ -1,14 +1,26 @@
 void main() {
-  String Name = 'Affan'; // Type = String
+  // String Name = 'Affan'; // Type = String
 
-  int Age = 20; // Type = int
+  // int Age = 20; // Type = int
 
-  double Height = 6.1;  // Type = double
+  // double Height = 6.1;  // Type = double
 
-  bool isMarried = false; // Type = bool
+  // bool isMarried = false; // Type = bool
 
-  print("My Name is $Name and Type is ${Name.runtimeType}");
-  print("My Age is $Age and Type is ${Age.runtimeType}");
-  print("My Height is $Height and Type is ${Height.runtimeType}");
-  print("My Marital Status is $isMarried and Type is ${isMarried.runtimeType}");
+  // print("My Name is $Name and Type is ${Name.runtimeType}");
+  // print("My Age is $Age and Type is ${Age.runtimeType}");
+  // print("My Height is $Height and Type is ${Height.runtimeType}");
+  // print("My Marital Status is $isMarried and Type is ${isMarried.runtimeType}");
+
+  // DIfference between dynamic and var
+
+  // var a = "Affan";
+  // print(a);
+
+  // a = 12; // This will give an error because 'a' is declared as a String and cannot be reassigned to an int type value because 'var' is a static and final type
+
+  dynamic b = 20;
+  print(b);
+
+  b = "Affan"; // This will not give an error because 'b' is declared as a dynamic type and can be reassigned to any type of value because 'dynamic' is a dynamic type
 }
