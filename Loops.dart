838a3f97ddc1b96acdd105a6056b7601e90f3ebb
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-13 09:46:23 +0500

    Dart Programming

--- Loops.dart
+++ Loops.dart
@@ -10,10 +10,18 @@ void main() {
   //   print(i);
   // }
 
-  for (int i = 1; i <= 5; i++) {
-    for (int j = 1; j <= i; j++) {
-      stdout.write("*");
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  for (int i = 1; i <= 10; i++) {
+    if (i > 5) {
+      print("Bss yahi tak");
+      break;
     }
-    print("");
+    print(i);
   }
 }
