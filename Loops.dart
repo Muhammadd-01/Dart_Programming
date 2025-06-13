2f621e8f2691926183ab3c550f808d2ed01a7d98
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-13 09:53:30 +0500

    Dart Programming

--- Loops.dart
+++ Loops.dart
@@ -2,26 +2,30 @@ import 'dart:io';
 
 void main() {
   //string interpolation
-  // for (int i = 1; i <= 10; i++) {
-  //   print("5 * $i = ${5 * i}");
-  // }
+  for (int i = 1; i <= 10; i++) {
+    // print("5 * $i = ${5 * i}");
+  }
 
-  // for (int i = 1; i <= 15; i+=2) {
-  //   print(i);
-  // }
+  for (int i = 1; i <= 15; i += 2) {
+    // print(i);
+  }
 
-  // for (int i = 1; i <= 5; i++) {
-  //   for (int j = 1; j <= i; j++) {
-  //     stdout.write("*");
-  //   }
-  //   print("");
-  // }
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+      // stdout.write("*");
+    }
+    // print("");
+  }
 
   for (int i = 1; i <= 10; i++) {
     if (i > 5) {
-      print("Bss yahi tak");
+      // print("Bss yahi tak");
       break;
     }
+    // print(i);
+  }
+
+  for (int i = 1; i <= 10; i++) {
     print(i);
   }
 }
