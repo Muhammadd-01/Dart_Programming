2f4ad78e0c4b1e01160424394d3f9f7959887f78
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-16 10:20:50 +0500

    Functions

--- Functions.dart
+++ Functions.dart
@@ -5,15 +5,19 @@ void main() {
   // int result = add(5);
   // print(add(5));
 
-  print("Enter your name:");
+  // print("Enter your name:");
   // String? name = stdin.readLineSync();
   // greet(name!);
 
-  print("Enter your age:");
-  int? age = int.parse(stdin.readLineSync()!);
+  // print("Enter your age:");
+  // int? age = int.parse(stdin.readLineSync()!);
 
-  checkAge(age);
+  // checkAge(age);
+
+  cityCheck("Affan", "Karachi");
 }
+
+//Arrow shorthand function
 
 int add(int a) => a + 10;
 
@@ -21,10 +25,18 @@ void greet(String name) {
   print('Hello, $name!');
 }
 
+//Input from user
+// Function to check age
 void checkAge(int age) {
   if (age >= 18) {
     print("You are an adult.");
   } else {
     print("you are minor.");
   }
+}
+
+//optional parameter function
+
+void cityCheck(String name, [String? city]) {
+  print("Hello $name from ${city ?? 'Unknown City'}");
 }
