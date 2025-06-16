107131c2d0bb78c0413ac3914b0a22f4d2e3e118
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-16 10:11:29 +0500

    Functions

--- Functions.dart
+++ Functions.dart
@@ -1,6 +1,30 @@
+import 'dart:ffi';
+import 'dart:io';
+
 void main() {
   // int result = add(5);
-  print(add(5));
+  // print(add(5));
+
+  print("Enter your name:");
+  // String? name = stdin.readLineSync();
+  // greet(name!);
+
+  print("Enter your age:");
+  int? age = int.parse(stdin.readLineSync()!);
+
+  checkAge(age);
 }
 
 int add(int a) => a + 10;
+
+void greet(String name) {
+  print('Hello, $name!');
+}
+
+void checkAge(int age) {
+  if (age >= 18) {
+    print("You are an adult.");
+  } else {
+    print("you are minor.");
+  }
+}
