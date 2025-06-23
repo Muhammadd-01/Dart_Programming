ad979b2e2e47718c0440f7f08bca63fe4a393c0f
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-23 10:04:03 +0500

    Dart Programming

---
+++ OOPs in Dart/Inheritance.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+class stdOne {
+  String name = "";
+  int age = 0;
+}
+
+class stdTwo extends stdOne {
+  String courseName = "";
+  int courseDuration = 0;
+}
+
+class stdThree extends stdTwo {
+  String universityName = "";
+  int universityDuration = 0;
+
+  void displayInfo() {
+    print(
+      "Hello, my name is $name and my age is $age.My course name is $courseName and course duration is $courseDuration monthsUniversity name is $universityName and universty duration is $universityDuration months",
+    );
+  }
+}
+
+void main() {
+  stdThree student = stdThree();
+  student.name = 'Affan';
+  student.age = 20;
+  student.courseName = 'Flutter';
+  student.courseDuration = 6;
+  student.universityName = 'University of Karachi';
+  student.universityDuration = 48;
+  student.displayInfo();
+}
