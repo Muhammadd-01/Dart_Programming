35a23a571dc702585459719a3e9512a694d27c44
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-23 09:14:43 +0500

    Advance JS

--- OOPs in Dart/Class.dart
+++ OOPs in Dart/Class.dart
@@ -7,9 +7,26 @@ class Person {
   }
 }
 
+class Info {
+  int stdId = 1;
+  String stdName = "";
+  int stdMarks = 0;
+
+  void showInfo() {
+    print(
+      "Student ID: $stdId, Student Name: $stdName, Student Marks: $stdMarks",
+    );
+  }
+}
+
 void main() {
-  Person p1 = Person();
-  p1.name = "Affan";
-  p1.age = 20;
-  p1.sayHello();
+  //   Person p1 = Person();
+  //   p1.name = "Affan";
+  //   p1.age = 20;
+  //   p1.sayHello();
+  Info std1 = Info();
+  std1.stdId = 1;
+  std1.stdName = 'Affan';
+  std1.stdMarks = 100;
+  std1.showInfo();
 }
