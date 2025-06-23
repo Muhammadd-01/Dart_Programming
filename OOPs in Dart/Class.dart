4c87ceaad4da13b4fc65f24418e538fe24a4cb70
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-23 09:10:07 +0500

    Advance JS

--- OOPs in Dart/Class.dart
+++ OOPs in Dart/Class.dart
@@ -1,3 +1,15 @@
-void main(){
-  
+class Person {
+  String name = "";
+  int age = 0;
+
+  void sayHello() {
+    print("Hello, my name is $name and my age is $age ");
+  }
+}
+
+void main() {
+  Person p1 = Person();
+  p1.name = "Affan";
+  p1.age = 20;
+  p1.sayHello();
 }
