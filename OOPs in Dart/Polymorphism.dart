07eb55c5b2c820df4d9ce723d4425ee1ce01fb5e
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-06-25 10:13:02 +0500

    Dart Programming

--- OOPs in Dart/Polymorphism.dart
+++ OOPs in Dart/Polymorphism.dart
@@ -1,1 +1,18 @@
 //Polymorphism in Dart
+
+class Student {
+  void Display() {
+    print("This is a Student class");
+  }
+}
+
+class Teacher extends Student {
+  void Display() {
+    print("This is a Teacher class");
+  }
+}
+
+void main() {
+  Teacher teacher = Teacher();
+  teacher.Display();
+}
